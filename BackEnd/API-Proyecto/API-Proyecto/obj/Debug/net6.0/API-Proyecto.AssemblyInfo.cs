5cd5a67ab8293a4a4fa81edae61842a5ff3441bf
Alex Blanco Benito <alexby20@gmail.com>
2026-03-17 11:16:41 +0100

    Problema de puerto arreglado

--- BackEnd/API-Proyecto/API-Proyecto/obj/Debug/net6.0/API-Proyecto.AssemblyInfo.cs
+++ BackEnd/API-Proyecto/API-Proyecto/obj/Debug/net6.0/API-Proyecto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API-Proyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cc65b26b7a1c4656ba1dccbd75b2657708b1e92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1a069d870c294de989c679b777c3841bc63f8b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API-Proyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API-Proyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
